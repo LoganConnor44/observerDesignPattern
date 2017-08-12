4996ecbbb61fda4ec9b14dc46622f9478413d65c
Logan Connor <loganconnor44@gmail.com>
2017-08-12 15:00:19 -0400

    initial commit

---
+++ src/subjects/ISubject.php
@@ -0,0 +1,25 @@
+<?php
+namespace WeatherORama;
+
+/**
+ * An interface for all subjects to pull from
+ */
+interface ISubject {
+
+	/**
+	 * An abstract method to add observers for any object that implements ISubject
+	 * @param object IObserver
+	 */
+	public function addObserver(IObserver $Observer);
+
+	/**
+	 * An abstract method to remove observers for any object that implements ISubject
+	 * @param object IObserver
+	 */
+	public function removeObserver(IObserver $Observer);
+
+	/**
+	 * An abstract method to notify observers for any object that implements ISubject
+	 */
+	public function notifyObservers();
+}
